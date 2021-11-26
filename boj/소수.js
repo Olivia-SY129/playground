bb5ece6798b00c7938ef8c32d11c85ced28953ee
Seungyeon Lee <sie9412@gmail.com>
2021-11-26 22:04:02 +0900

    BOJ 알고리즘

---
+++ boj/소수.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const start = +input[0];
+const end = +input[1];
+
+const isPrime = num => {
+  if (num === 1) return false;
+  const sqrt = Math.floor(Math.sqrt(num));
+  for (let i = 2; i <= sqrt; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+let sum = 0;
+let min = Infinity;
+for (let i = start; i <= end; i++) {
+  if (!isPrime(i)) continue;
+  sum += i;
+  min = Math.min(min, i);
+}
+
+if (sum === 0) console.log(-1);
+else console.log(sum + '\n' + min);
---
+++ boj/소수찾기.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const numbers = input[1].split(' ').map(str => +str);
+
+const isPrime = num => {
+  if (num === 1) return false;
+  const sqrt = Math.floor(Math.sqrt(num));
+  for (let i = 2; i <= sqrt; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+const prime = nums => nums.filter(num => isPrime(num)).length;
+
+console.log(prime(numbers));
